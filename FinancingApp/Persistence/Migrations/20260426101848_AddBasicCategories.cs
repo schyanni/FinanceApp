a97008faf2097c1e3895e395581962faa622b1e7
Yannick S <schyanni@pm.me>
2026-04-26 12:36:54 +0200

    Add DB migration with some basic categories to get started
    Note: most likely these static categories will again be removed once a
    dynamic category creation feature has been implemented

---
+++ FinancingApp/Persistence/Migrations/20260426101848_AddBasicCategories.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancingApp.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBasicCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories (Name) " +
+                "VALUES ('Einkommen'), ('Gesundheit'), ('Lebenshaltung'), ('Lohn'), ('Sonstiges');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
