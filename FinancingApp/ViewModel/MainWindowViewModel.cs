87c80337989cc9a24f8fa30fcac9c264c8854a4f
Yannick S <schyanni@pm.me>
2026-05-02 12:12:51 +0200

    Menubar with import data menu item

--- FinancingApp/ViewModel/MainWindowViewModel.cs
+++ FinancingApp/ViewModel/MainWindowViewModel.cs
@@ -1,24 +1,37 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System.Windows.Input;
+using FinancingApp.Domain;
+using Microsoft.Win32;
+using System.Windows;
 
 namespace FinancingApp.ViewModel
 {
     public class MainWindowViewModel : ObservableObject
     {
-        public MainWindowViewModel(TransactionCreationViewModel transactionCreationViewModel, TransactionsListViewModel transactionsListViewModel)
+        private readonly CsvImportService _csvImportService;
+
+        public MainWindowViewModel(
+            TransactionCreationViewModel transactionCreationViewModel,
+            TransactionsListViewModel transactionsListViewModel,
+            CsvImportService csvImportService)
         {
             TransactionCreationViewModel = transactionCreationViewModel;
             TransactionsListViewModel = transactionsListViewModel;
+            _csvImportService = csvImportService;
 
             OpenTransactionCreationCommand = new RelayCommand(OpenTransactionCreation);
             LoadInitialDataCommand = new AsyncRelayCommand(async () => await TransactionsListViewModel.LoadDataFromDbAsync());
-
+            ImportDataCommand = new AsyncRelayCommand(ImportDataAsync);
+            ExitCommand = new RelayCommand(() => { });
         }
 
         public IRelayCommand OpenTransactionCreationCommand { get; }
 
         public IAsyncRelayCommand LoadInitialDataCommand { get; }
+
+        public IAsyncRelayCommand ImportDataCommand { get; }
+
+        public IRelayCommand ExitCommand { get; }
 
         public TransactionCreationViewModel TransactionCreationViewModel { get; }
 
@@ -27,6 +40,22 @@ namespace FinancingApp.ViewModel
         private void OpenTransactionCreation()
         {
             // Logic to open the transaction creation view
+        }
+
+        private async Task ImportDataAsync()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "CSV-Dateien (*.csv)|*.csv",
+            };
+
+            if (dialog.ShowDialog() != true)
+            {
+                return;
+            }
+
+            var (imported, skipped) = await _csvImportService.ImportAsync(dialog.FileName);
+            MessageBox.Show($"{imported} Transaktionen importiert, {skipped} übersprungen.");
         }
     }
 }
